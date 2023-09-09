cf7f8dc38a2a07bba4a424f5a72199ba6102273c
sagrario <sagrariocacaris2004@gmail.com>
2023-09-09 10:51:27 -0500

    commit 12

--- Hotel/js/sistema.js
+++ Hotel/js/sistema.js
@@ -10,3 +10,38 @@ function seleccioner(link){
 
 links.className = "selected";
 }
+
+document.getElementById('registro-form').addEventListener('submit', function(evento) {
+    evento.preventDefault();
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var correo = document.getElementById('correo').value;
+    var tipoDocumento = document.getElementById('tipoDocumento').value;
+    var aceptarCondiciones = document.getElementById('aceptarCondiciones').checked;
+
+    if (nombre.trim() === '') {
+        alert('Por favor, ingresa tu nombre');
+        return;
+    }
+    if (!/^\S+.\S+\@\S+$/.test(correo)) {
+        alert('Ingresa un correo electrónico válido');
+        return;
+    }
+    alert('Formulario validado con éxito. ¡Gracias por registrarte!');
+});
+
+document.getElementById('inicio-form').addEventListener('submit', function(evento) {
+    evento.preventDefault();
+    var nombre = document.getElementById('nombre').value;
+    var correo = document.getElementById('contr').value;
+
+    if (nombre.trim() === '') {
+        alert('Por favor, un usuario');
+        return;
+    }
+    if (!/^\S+@\S+\.\S+$/.test(contr)) {
+        alert('ds');
+        return;
+    }
+    alert('Formulario validado con éxito. ¡Gracias por registrarte!');
+});
