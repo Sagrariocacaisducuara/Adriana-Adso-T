844a109151dd8b77506d637711c8f27d12727361
sagrario <sagrariocacaris2004@gmail.com>
2023-09-10 07:55:45 -0500

    commit 13

--- Hotel/js/sistema.js
+++ Hotel/js/sistema.js
@@ -11,37 +11,58 @@ function seleccioner(link){
 links.className = "selected";
 }
 
-document.getElementById('registro-form').addEventListener('submit', function(evento) {
-    evento.preventDefault();
-    var nombre = document.getElementById('nombre').value;
-    var apellido = document.getElementById('apellido').value;
-    var correo = document.getElementById('correo').value;
-    var tipoDocumento = document.getElementById('tipoDocumento').value;
-    var aceptarCondiciones = document.getElementById('aceptarCondiciones').checked;
+function validarFormularioContacto() {
+    var nombre1 = document.getElementById("nombre1").value;
+    var apellido1 = document.getElementById("apellido1").value;
+    var correo1 = document.getElementById("correo1").value;
+    var mensaje = document.getElementById("mensaje").value;
 
-    if (nombre.trim() === '') {
-        alert('Por favor, ingresa tu nombre');
-        return;
+    if (nombre1 === "" || apellido1 === "" || correo1 === "" || mensaje === "") {
+        alert("Por favor, complete todos los campos del formulario de contacto.");
+        return false;
     }
-    if (!/^\S+.\S+\@\S+$/.test(correo)) {
-        alert('Ingresa un correo electrónico válido');
-        return;
-    }
-    alert('Formulario validado con éxito. ¡Gracias por registrarte!');
-});
+    alert('Formulario validado con éxito. ¡Gracias!');
+}
+
 
 document.getElementById('inicio-form').addEventListener('submit', function(evento) {
     evento.preventDefault();
     var nombre = document.getElementById('nombre').value;
-    var correo = document.getElementById('contr').value;
+    var contr = document.getElementById('contr').value;
 
     if (nombre.trim() === '') {
-        alert('Por favor, un usuario');
+        alert('Por favor, ingresa tu usuario.');
         return;
     }
-    if (!/^\S+@\S+\.\S+$/.test(contr)) {
-        alert('ds');
+    if (contr.trim() === '') {
+        alert('Por favor, ingresa tu contraseña.');
         return;
     }
-    alert('Formulario validado con éxito. ¡Gracias por registrarte!');
+
+    alert('Formulario validado con éxito. ¡Gracias por iniciar sesión!');
 });
+
+/*function validarFormularioRegistro() {
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var correo = document.getElementById("correo").value;
+    var tipoDocumento = document.getElementById("tipoDocumento").value;
+    var aceptarCondiciones = document.getElementById("aceptarCondiciones").checked;
+
+    if (nombre === "" || apellido === "" || correo === "") {
+        alert("Por favor, complete todos los campos del formulario de registro.");
+        return false;
+    }
+
+    if (!aceptarCondiciones) {
+        alert("Debe aceptar las condiciones para registrarse.");
+        return false;
+    }
+
+    // Puedes agregar más validaciones según tus necesidades
+
+    return true;
+}*/
+
+
+
